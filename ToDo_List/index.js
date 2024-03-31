b5071c6136873cfca83c2eb0c39f3fcb05cb78ee
Ayesha A.Qadir <ayeshaabdulqadir07@gmail.com>
2024-03-31 17:39:04 +0500

    ToDo_List

---
+++ ToDo_List/index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: "What you want to add in your ToDos?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "Do you want to add more",
+            default: false,
+        }
+    ]);
+    todos.push(addTask.todo);
+    loop = addTask.addMore;
+    console.log(todos);
+}
+if (todos.length > 0) {
+    console.log("Your ToDo List:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("no todos found");
+}
