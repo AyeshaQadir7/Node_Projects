83c5e598b080a70e80755ffb652b23c39b3a0bb9
Ayesha A.Qadir <ayeshaabdulqadir07@gmail.com>
2024-04-07 13:35:41 +0500

    BMI_Calculator

---
+++ BMI-calculator/index.js
@@ -0,0 +1,4 @@
+var weightInKg = 70; //70kg
+var heightInMeters = 1.75; //1.75m
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Your BMI is ".concat(bmi));
---
+++ BMI-calculator/index.ts
@@ -0,0 +1,5 @@
+let weightInKg = 70; //70kg
+let heightInMeters = 1.75; //1.75m
+
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}`);
