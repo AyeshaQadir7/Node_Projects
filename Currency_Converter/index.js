a7fb082089a5891e7df7fd9192d03652f499d333
Ayesha A.Qadir <ayeshaabdulqadir07@gmail.com>
2024-04-07 13:31:14 +0500

    Currency_Converter

---
+++ Currency_Converter/index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter from Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "Enter to Currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: 'Enter your amount',
+        type: 'number',
+    }
+]);
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount; //USD based currency
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
