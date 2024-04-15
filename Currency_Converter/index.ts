dcb5e7e59bb156f95a229dbda168aa3e2e430822
Ayesha A.Qadir <ayeshaabdulqadir07@gmail.com>
2024-04-15 18:01:12 +0500

    Currency_Converter

--- Currency_Converter/index.ts
+++ Currency_Converter/index.ts
@@ -1,29 +1,41 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer"
+import chalk from 'chalk';
+import clear from 'clear'
+import figlet from 'figlet'
+
+clear();
+console.log(chalk.hex('#FF7F11')(figlet.textSync('Welcome', { horizontalLayout: 'full' })));
+console.log(chalk.hex('#FFFDD0').bold("\n\t\t to Currency Converter\n"))
 
 const currency: any = {
     USD: 1, //Base currency
-    EUR: 0.91,
-    GBP: 0.76,
-    INR: 74.57,
-    PKR: 280,
+    EUR: 0.91, //European Euro
+    GBP: 0.76, //British Pound
+    INR: 74.57, // Indian Rupee
+    PKR: 280, //Pakistani Rupee
+    JYP: 151, // Japanes Yen
+    CAD: 1.3, //Canadian Dollar
+    KRW: 1351, //Korean Won
 }
 let userAnswer = await inquirer.prompt(
     [
         {
             name: 'from',
-            message: "Enter from Currency",
+            message: "Enter from Currency:",
             type: 'list',
-            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'JYP', 'CAD', 'KRW']
         },
         {
             name: 'to',
-            message: "Enter to Currency",
+            message: "Enter to Currency:",
             type: 'list',
-            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+            choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR', 'JYP', 'CAD', 'KRW']
         },
         {
             name: 'amount',
-            message: 'Enter your amount',
+            message: 'Enter your amount:',
             type: 'number',
         }
 
@@ -33,6 +45,10 @@ let userAnswer = await inquirer.prompt(
 let fromAmount = currency[userAnswer.from]
 let toAmount = currency[userAnswer.to]
 let amount = userAnswer.amount
+
+//Formula of conversion
 let baseAmount = amount/fromAmount  //USD based currency
 let convertedAmount = baseAmount * toAmount
-console.log(convertedAmount)
+
+console.log(chalk.hex('#FF7F11').bold(`\n | Your Converted Amount is: ${convertedAmount} | \n`));
+console.log(chalk.hex('#FF7F11').bold(" \n Thankyou! \n "))
